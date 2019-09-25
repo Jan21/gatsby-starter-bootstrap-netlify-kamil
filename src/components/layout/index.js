1b10b105c1b62e31470d43d41ccb32885b72e25e
Jaruska Hulova <jaruska.hulova@gmail.com>
2019-09-25 20:24:36 +0200

    added video bcgr 5

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -12,7 +12,7 @@ export default ({ children }) => (
   <div className="text-wrap">
         <div className='pageContent'>{children}</div>
   </div>
-  <video className="jumbo-video" poster="video_jpg.jpg" autoplay="true" loop muted="">
+  <video className="jumbo-video"  playsinline autoplay poster="video_jpg.jpg" loop muted="">
   <source src="bcgr_video.mp4" type="video/mp4"></source>
 </video>
 </div>
