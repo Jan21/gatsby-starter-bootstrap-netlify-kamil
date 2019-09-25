682e0e19851f99d06a5a11d820c4c71cc257944a
Jaruska Hulova <jaruska.hulova@gmail.com>
2019-09-25 20:00:43 +0200

    added video

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -8,7 +8,12 @@ import 'prismjs/themes/prism-twilight.css'
 import './index.scss'
 
 export default ({ children }) => (
-  <div className="bg">
+  <div className="jumbo">
+  <div className="text-wrap">
         <div className='pageContent'>{children}</div>
   </div>
+  <video className="jumbo-video" poster="./backgr.jpg" autoplay="true" loop muted="">
+  <source src="bcgr_video.mp4" type="video/mp4"></source>
+</video>
+</div>
 ) 
